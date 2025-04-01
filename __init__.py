a31c0380e9f66b7c812663cb924bd9a4637646b0
selenemoon78 <asmaa.bahammou@edu.esiee-it.fr>
2025-04-01 14:28:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def mongraphique():
+def monhisto():
     return render_template("histogramme.html")
   
 if __name__ == "__main__":
